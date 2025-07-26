8be220b9ffb11cf3304a509c44afedb74f9b7e80
Attala Bharath Vardhan <166591003+BHARATHVARD@users.noreply.github.com>
2025-07-26 14:36:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
-//function to fetch data from api
+// Add your OMDB API Key here or use environment variable
+const key = "ac6311d7"; // Replace with process.env.NEXT_PUBLIC_API_KEY if using React/Next.js
 
+// Function to fetch data from API
 let getMovie = () => {
-    let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-    //if input field is empty
+    let movieName = movieNameRef.value.trim();
 
-    if (movieName.length <= 0) {
-        result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
+    // If input field is empty
+    if (movieName.length === 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a movie name</h3>`;
+        return;
     }
 
-    //if input isn't empty
-    else {
-        fetch(url).then((resp) => resp.json()).then((data) => {
-            //if movie exist in database
-            if (data.Response == "True") {
+    // Construct API URL (use HTTPS and encode query)
+    let url = ` http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+
+    // Fetch API
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            if (data.Response === "True") {
                 result.innerHTML = `
                     <div class="info">
-                        <img src=${data.Poster} class="poster">
+                        <img src="${data.Poster}" class="poster">
                         <div>
                             <h2>${data.Title}</h2>
                             <div class="rating">
@@ -42,18 +47,13 @@ let getMovie = () => {
                     <h3>Cast:</h3>
                     <p>${data.Actors}</p>
                 `;
-            }
-
-            //if movie doesn't exist in database
-            else {
+            } else {
                 result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
             }
         })
-            //if error occurs
-            .catch(() => {
-                result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
-            });
-    }
+        .catch(() => {
+            result.innerHTML = `<h3 class="msg">Error Occurred</h3>`;
+        });
 };
 
 searchBtn.addEventListener("click", getMovie);
